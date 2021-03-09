652579778c8bf0e061bf379ec6be4381164972a7
tomi1710 <2294@holbertonschool.com>
2021-03-09 13:14:54 +0000

    add files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t = malloc(sizeof(binary_tree_t));
+    if (binary_tree_t == NULL)
+        return(NULL);
+    
+    binary_tree_t parent = Node(value);
+    return(Node);
+}
