b2ace90aae2305f9ce3660c451354eb32dee02d2
tomi1710 <2294@holbertonschool.com>
2021-03-10 13:12:43 +0000

    fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - goes through a binary tree using in-order traversal
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
  * @tree: tree to print
  * @func: funcfuncfunc
  */
