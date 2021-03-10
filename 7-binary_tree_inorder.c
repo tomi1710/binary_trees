509f3b1d4d55f3bdfe2973845b13ed371a2dd0ec
tomi1710 <2294@holbertonschool.com>
2021-03-10 13:07:51 +0000

    fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,6 +9,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 	binary_tree_inorder(tree->left, func);
-    func(tree->n);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
