fb2c653e0765a86c99a56a8260b1efab87fad093
tomi1710 <2294@holbertonschool.com>
2021-03-10 21:01:35 +0000

    fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,16 +6,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    int a;
+	int a;
 
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-    
-    a = node->n;
-    node = node->parent;
-    if (node->left == NULL || node->right == NULL)
-        return (NULL);
-    if (node->left->n == a)
-        return (node->right);
-    return (node->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	a = node->n;
+	node = node->parent;
+	if (node->left == NULL || node->right == NULL)
+		return (NULL);
+	if (node->left->n == a)
+		return (node->right);
+	return (node->left);
 }
